070647ef93aa89f1ad405dcc4457d2ce2025faf0
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-05-16 11:43:30 +0500

    finish 3 attempt

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
-
+import { readFileSync } from 'node:fs';
 const { Command } = require('commander');
 const program = new Command();
+
+const command = () => {
+   console.log(readFileSync());
+   //console.log(filepath1, filepath2);
+};
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .option('-v, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>')
+  .action(command)
 program.parse();
+ 
