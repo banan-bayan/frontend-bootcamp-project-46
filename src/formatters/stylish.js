d79edb3f079294fd92e7735fce24950ee5cf7551
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-05-22 13:23:26 +0500

    add perfect spaces

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,17 +1,18 @@
 import _ from 'lodash';
 import statuses from '../../constants.js';
 
-const spaces = 2;
+const space = 4;
 const sign = ' ';
+
 const stringify = (obj, depth) => {
   const iter = (node, level) => {
     if (!_.isObject(node)) {
       return `${node}`;
     }
     const entries = Object.entries(node);
-    const result = entries.map(([key, value]) => `${sign.repeat(spaces * level)}${key}: ${iter(value, level + 1)}`);
+    const result = entries.map(([key, value]) => `${sign.repeat(space * level + space)}${key}: ${iter(value, level + 1)}`);
     const strings = `${result.join('\n')}`;
-    return (`{\n${sign.repeat(spaces * level - spaces)}${strings}\n${sign.repeat(spaces * level)}}`);
+    return (`{\n${strings}\n${sign.repeat(space * level)}}`);
   };
   return iter(obj, depth);
 };
@@ -21,17 +22,16 @@ export default (tree) => {
     const strings = [];
     node.forEach((subSt) => {
       if (subSt.status === 'changed') {
-        strings.push(`${sign.repeat(spaces * level)}- ${subSt.key}: ${stringify(subSt.value1, level + 1)}\n
-        ${sign.repeat(spaces * level)}+ ${subSt.key}: ${stringify(subSt.value2, level + 1)}`);
+        strings.push(`${sign.repeat(space * level + (space / 2))}- ${subSt.key}: ${stringify(subSt.value1, level + 1)}\n${sign.repeat(space * level + (space / 2))}+ ${subSt.key}: ${stringify(subSt.value2, level + 1)}`);
       } else if (subSt.status === 'nested') {
-        strings.push(`${sign.repeat(spaces * level)}${subSt.key}: ${iter(subSt.children, level + 1)}`);
+        strings.push(`${sign.repeat(space + level * space)}${subSt.key}: ${iter(subSt.children, level + 1)}`);
       } else {
-        strings.push(`${sign.repeat(spaces * level)}${statuses[subSt.status]} ${subSt.key}: ${stringify(subSt.value, level + 1)}`);
+        strings.push(`${sign.repeat(space * level + (space / 2))}${statuses[subSt.status]} ${subSt.key}: ${stringify(subSt.value, level + 1)}`);
       }
     });
-    const obString = `{\n${strings.join('\n')}\n${sign.repeat(spaces * level - spaces)}}`;
+    const obString = `{\n${strings.join('\n')}\n${sign.repeat(level * space)}}`;
     return obString;
   };
 
-  return iter(tree, 1);
+  return iter(tree, 0);
 };
