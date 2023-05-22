8e3c44164aa455d4e9798061fc7fe716d0fa5828
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-05-22 17:26:42 +0500

    add test for stylish

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,19 @@
 // import { expect, test } from '@jest/globals';
 
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import path, { dirname } from 'path';
+// import getFormattedTree from '../src/formatters/stylish.js';
 import { readFileSync } from 'node:fs';
 import { test, expect } from '@jest/globals';
 import getStringArr from '../src/index.js';
-import { fileYml, stringArr } from '../constants.js';
+import { stringArr, fileYml } from '../constants.js';
 import getParseFile from '../src/parser.js';
+
+const pathTxt = './__fixtures__/tree.txt';
 
 test('testString', () => {
   expect(getStringArr('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(stringArr);
+  expect(getStringArr('./__fixtures__/treeFile1.json', './__fixtures__/treeFile2.json')).toEqual(readFileSync(pathTxt, 'utf8'));
 });
 
 test('testFormat', () => {
@@ -17,10 +21,14 @@ test('testFormat', () => {
   expect(getParseFile('./__fixtures__/file1.yml')).toEqual(fileYml);
 });
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// test('testTree', () => {
+// expect(getFormattedTree(getStringArr('./__fixtures__/file1.json',
+// './__fixtures__/file2.json'))).toEqual(stringTree);
+// });
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
-console.log(readFile);
+// const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+// const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+// console.log(readFile);
 // readFile('expected_file.json');
