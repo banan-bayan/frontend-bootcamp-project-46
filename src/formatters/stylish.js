838dc11e734d47d924eff736e26fd3ffdf1d8fd2
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-05-22 11:33:39 +0500

    rename consts

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,17 +1,17 @@
 import _ from 'lodash';
 import statuses from '../../constants.js';
 
-const countRepeat = 2;
-const symbols = ' ';
+const spaces = 2;
+const sign = ' ';
 const stringify = (obj, depth) => {
   const iter = (node, level) => {
     if (!_.isObject(node)) {
       return `${node}`;
     }
     const entries = Object.entries(node);
-    const result = entries.map(([key, value]) => `${symbols.repeat(countRepeat * level)}${key}: ${iter(value, level + 1)}`);
+    const result = entries.map(([key, value]) => `${sign.repeat(spaces * level)}${key}: ${iter(value, level + 1)}`);
     const strings = `${result.join('\n')}`;
-    return (`{\n${symbols.repeat(countRepeat * level - countRepeat)}${strings}\n${symbols.repeat(countRepeat * level)}}`);
+    return (`{\n${sign.repeat(spaces * level - spaces)}${strings}\n${sign.repeat(spaces * level)}}`);
   };
   return iter(obj, depth);
 };
@@ -21,15 +21,15 @@ export default (tree) => {
     const strings = [];
     node.forEach((subSt) => {
       if (subSt.status === 'changed') {
-        strings.push(`${symbols.repeat(countRepeat * level)}- ${subSt.key}: ${stringify(subSt.value1, level + 1)}\n
-        ${symbols.repeat(countRepeat * level)}+ ${subSt.key}: ${stringify(subSt.value2, level + 1)}`);
+        strings.push(`${sign.repeat(spaces * level)}- ${subSt.key}: ${stringify(subSt.value1, level + 1)}\n
+        ${sign.repeat(spaces * level)}+ ${subSt.key}: ${stringify(subSt.value2, level + 1)}`);
       } else if (subSt.status === 'nested') {
-        strings.push(`${symbols.repeat(countRepeat * level)}${subSt.key}: ${iter(subSt.children, level + 1)}`);
+        strings.push(`${sign.repeat(spaces * level)}${subSt.key}: ${iter(subSt.children, level + 1)}`);
       } else {
-        strings.push(`${symbols.repeat(countRepeat * level)}${statuses[subSt.status]} ${subSt.key}: ${stringify(subSt.value, level + 1)}`);
+        strings.push(`${sign.repeat(spaces * level)}${statuses[subSt.status]} ${subSt.key}: ${stringify(subSt.value, level + 1)}`);
       }
     });
-    const obString = `{\n${strings.join('\n')}\n${symbols.repeat(countRepeat * level - countRepeat)}}`;
+    const obString = `{\n${strings.join('\n')}\n${sign.repeat(spaces * level - spaces)}}`;
     return obString;
   };
 
