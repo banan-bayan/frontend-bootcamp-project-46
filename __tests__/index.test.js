bef44602562791d2f78686667480e39f78b79c4f
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-05-23 22:24:00 +0500

    add tests again

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,20 +2,29 @@ import { readFileSync } from 'node:fs';
 import { test, expect } from '@jest/globals';
 import getStringArr from '../src/index.js';
 import { stringArr } from '../constants.js';
-// import getParseFile from '../src/parser.js'; , fileYml
 
 const pathTxtStylish = './__fixtures__/tree.txt';
 const pathTxtPlain = './__fixtures__/plain.txt';
 const pathTxtJson = './__fixtures__/json.txt';
+const pathTreeJson1 = './__fixtures__/treeFile1.json';
+const pathTreeJson2 = './__fixtures__/treeFile2.json';
+const pathTreeYml1 = './__fixtures__/treeFile1.yml';
+const pathTreeYml2 = './__fixtures__/treeFile2.yml';
+const fileJson1 = './__fixtures__/file1.json';
+const fileJson2 = './__fixtures__/file2.json';
+const fileYml1 = './__fixtures__/file1.yml';
+const fileYml2 = './__fixtures__/file2.yml';
 
-test('testString', () => {
-  expect(getStringArr('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(stringArr);
-  expect(getStringArr('./__fixtures__/treeFile1.json', './__fixtures__/treeFile2.json')).toEqual(readFileSync(pathTxtStylish, 'utf8'));
-  expect(getStringArr('./__fixtures__/treeFile1.json', './__fixtures__/treeFile2.json', 'plain')).toEqual(readFileSync(pathTxtPlain, 'utf8'));
-  expect(getStringArr('./__fixtures__/treeFile1.json', './__fixtures__/treeFile2.json', 'json')).toEqual(readFileSync(pathTxtJson, 'utf8'));
+test('testJson', () => {
+  expect(getStringArr(fileJson1, fileJson2)).toEqual(stringArr);
+  expect(getStringArr(pathTreeJson1, pathTreeJson2)).toEqual(readFileSync(pathTxtStylish, 'utf8'));
+  expect(getStringArr(pathTreeJson1, pathTreeJson2, 'json')).toEqual(readFileSync(pathTxtJson, 'utf8'));
 });
-
-// test('testFormat', () => {
-//   expect(getParseFile('')).toEqual(null);
-//   expect(getParseFile('./__fixtures__/file1.yml')).toEqual(fileYml);
-// });
+test('testYml', () => {
+  expect(getStringArr(fileYml1, fileYml2)).toEqual(stringArr);
+  expect(getStringArr(pathTreeYml1, pathTreeYml2)).toEqual(readFileSync(pathTxtStylish, 'utf8'));
+});
+test('testPlain', () => {
+  expect(getStringArr(pathTreeJson1, pathTreeJson2, 'plain')).toEqual(readFileSync(pathTxtPlain, 'utf8'));
+  expect(getStringArr(pathTreeYml1, pathTreeYml2, 'plain')).toEqual(readFileSync(pathTxtPlain, 'utf8'));
+});
