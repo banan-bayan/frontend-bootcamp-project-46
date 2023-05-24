b74b7aad08909321b444324459340a76bb784403
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-05-24 10:12:51 +0500

    rename tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -15,16 +15,16 @@ const fileJson2 = './__fixtures__/file2.json';
 const fileYml1 = './__fixtures__/file1.yml';
 const fileYml2 = './__fixtures__/file2.yml';
 
-test('testJson', () => {
+test('test json', () => {
   expect(getStringArr(fileJson1, fileJson2)).toEqual(stringArr);
   expect(getStringArr(pathTreeJson1, pathTreeJson2)).toEqual(readFileSync(pathTxtStylish, 'utf8'));
   expect(getStringArr(pathTreeJson1, pathTreeJson2, 'json')).toEqual(readFileSync(pathTxtJson, 'utf8'));
 });
-test('testYml', () => {
+test('test yml', () => {
   expect(getStringArr(fileYml1, fileYml2)).toEqual(stringArr);
   expect(getStringArr(pathTreeYml1, pathTreeYml2)).toEqual(readFileSync(pathTxtStylish, 'utf8'));
 });
-test('testPlain', () => {
+test('test plain', () => {
   expect(getStringArr(pathTreeJson1, pathTreeJson2, 'plain')).toEqual(readFileSync(pathTxtPlain, 'utf8'));
   expect(getStringArr(pathTreeYml1, pathTreeYml2, 'plain')).toEqual(readFileSync(pathTxtPlain, 'utf8'));
 });
