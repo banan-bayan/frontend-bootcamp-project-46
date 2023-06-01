2f303eeef5b45c0962e8172d98ea7b122304f865
banan-bayan <88555875+banan-bayan@users.noreply.github.com>
2023-06-01 17:30:16 +0500

    refact tests, fun compareObj, and del useless fils

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,6 @@
 import { readFileSync } from 'node:fs';
 import { test, expect } from '@jest/globals';
 import getStringArr from '../src/index.js';
-import stringArr from '../__fixtures__/stringArr.js';
 
 const pathTxtStylish = './__fixtures__/tree.txt';
 const pathTxtPlain = './__fixtures__/plain.txt';
@@ -10,21 +9,17 @@ const pathTreeJson1 = './__fixtures__/treeFile1.json';
 const pathTreeJson2 = './__fixtures__/treeFile2.json';
 const pathTreeYml1 = './__fixtures__/treeFile1.yml';
 const pathTreeYml2 = './__fixtures__/treeFile2.yml';
-const fileJson1 = './__fixtures__/file1.json';
-const fileJson2 = './__fixtures__/file2.json';
-const fileYml1 = './__fixtures__/file1.yml';
-const fileYml2 = './__fixtures__/file2.yml';
 
-test('testJson', () => {
-  expect(getStringArr(fileJson1, fileJson2)).toEqual(stringArr);
-  expect(getStringArr(pathTreeJson1, pathTreeJson2)).toEqual(readFileSync(pathTxtStylish, 'utf8'));
-  expect(getStringArr(pathTreeJson1, pathTreeJson2, 'json')).toEqual(readFileSync(pathTxtJson, 'utf8'));
+test.each([
+  [pathTreeJson1, pathTreeJson2, 'stylish', pathTxtStylish],
+  [pathTreeJson1, pathTreeJson2, 'json', pathTxtJson],
+  [pathTreeYml1, pathTreeYml2, 'stylish', pathTxtStylish],
+])('testTree(%p, %p, %p)', (file1, file2, format, expected) => {
+  expect(getStringArr(file1, file2, format)).toEqual(readFileSync(expected, 'utf-8'));
 });
-test('testYml', () => {
-  expect(getStringArr(fileYml1, fileYml2)).toEqual(stringArr);
-  expect(getStringArr(pathTreeYml1, pathTreeYml2)).toEqual(readFileSync(pathTxtStylish, 'utf8'));
-});
-test('testPlain', () => {
-  expect(getStringArr(pathTreeJson1, pathTreeJson2, 'plain')).toEqual(readFileSync(pathTxtPlain, 'utf8'));
-  expect(getStringArr(pathTreeYml1, pathTreeYml2, 'plain')).toEqual(readFileSync(pathTxtPlain, 'utf8'));
+test.each([
+  [pathTreeJson1, pathTreeJson2, 'plain', pathTxtPlain],
+  [pathTreeYml1, pathTreeYml2, 'plain', pathTxtPlain],
+])('testPlain(%p, %p, %p)', (file1, file2, format, expected) => {
+  expect(getStringArr(file1, file2, format)).toEqual(readFileSync(expected, 'utf-8'));
 });
--- src/compareObjects.js
+++ src/compareObjects.js
@@ -4,10 +4,18 @@ const compareObjects = (obj1, obj2) => {
   const keys = _.union([...Object.keys(obj2), ...Object.keys(obj1)]);
   const sortKeys = _.sortBy(keys);
   const resultArr = sortKeys.map((key) => {
-    if (_.isObject(obj1[key]) && (_.isObject(obj2[key]))) return ({ status: 'nested', key, children: compareObjects(obj1[key], obj2[key]) });
-    if (obj1[key] === obj2[key]) return ({ status: 'unchanged', key, value: obj1[key] });
-    if (_.has(obj1, key) && !_.has(obj2, key)) return ({ status: 'deleted', key, value: obj1[key] });
-    if (!_.has(obj1, key) && _.has(obj2, key)) return ({ status: 'added', key, value: obj2[key] });
+    if (_.isObject(obj1[key]) && (_.isObject(obj2[key]))) {
+      return ({ status: 'nested', key, children: compareObjects(obj1[key], obj2[key]) });
+    }
+    if (obj1[key] === obj2[key]) {
+      return ({ status: 'unchanged', key, value: obj1[key] });
+    }
+    if (!_.has(obj2, key)) {
+      return ({ status: 'deleted', key, value: obj1[key] });
+    }
+    if (!_.has(obj1, key)) {
+      return ({ status: 'added', key, value: obj2[key] });
+    }
     if (obj1[key] !== obj2[key]) {
       return ({
         status: 'changed', key, value1: obj1[key], value2: obj2[key],
